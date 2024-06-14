93faf9173527a53dec622ae81ee6b2989e79961a
David Miguel Andrés <damigand700@gmail.com>
2024-06-14 17:01:53 +0200

    tres en raya completado
    tres en raya funciona perfectamente excepto una cosa visual, jugable por el momento, no he testeado mucho

--- src/components/ThreeInRowComponent/threeinrow.js
+++ src/components/ThreeInRowComponent/threeinrow.js
@@ -1,8 +1,16 @@
 import './threeinrow.css';
 
-const currentGame = [];
+const winningMap = ['123', '456', '789', '147', '258', '369', '159', '357'];
+var currentGame = [];
+var winnerCells = [];
+var circleScore = 0;
+var crossScore = 0;
+//Turno. 1 = X, 0 = O
+var currentTurn;
+var hasGameStarted = false;
 
-export function threeInRowGame(game) {
+export function threeInRowGame() {
+   loadGame();
    return `
         <article class="game-threeinrow spacing">
             <div class="threeinrow-score">
@@ -24,11 +32,11 @@ function drawScore() {
     <div class="score-board">
       <div class="player-1">
         <img src="circle.png" />
-        <p>0</p>
+        <p>${circleScore}</p>
       </div> 
       <p> - </p>
       <div class="player-2">
-       <p>0</p>
+       <p>${crossScore}</p>
         <img src="cross.png" />
       </div>
     </div>
@@ -38,27 +46,48 @@ function drawScore() {
 function drawBoard() {
    var elements = '';
    var cellCounter = 1;
-   for (var a = 1; a < 4; a++) {
-      for (var b = 1; b < 4; b++) {
-         elements += drawBoardCell(a, b, cellCounter);
+   if (!currentGame || currentGame.length < 1) {
+      for (var a = 1; a < 4; a++) {
+         for (var b = 1; b < 4; b++) {
+            elements += drawBoardCell(a, b, cellCounter);
+            cellCounter++;
+         }
+      }
+   } else {
+      for (const cell of currentGame) {
+         elements += drawBoardCell(cell.row, cell.column, cellCounter, cell.symbol, true, cell);
          cellCounter++;
       }
    }
    return `
-    <div class="threeinrow-cells">
-      ${elements}
+      <div class="threeinrow-cells">
+         ${elements}
       </div>
    `;
 }
 
-function drawBoardCell(row, column, cell) {
-   const id = 'cell-' + cell;
-   currentGame.push({ id: id, symbol: '' });
+function drawBoardCell(row, column, cellNumber, symbol = -1, loading = false, cell) {
+   const id = 'cell-' + cellNumber;
+   if (!loading) {
+      currentGame.push({ id: id, symbol: -1, row: row, column: column });
+      return `
+         <div class="cell row-${row} col-${column}" id="${id}">
+         </div>
+      `;
+   } else {
+      var imgString = '';
+      if (symbol == 1) imgString = `<img class="cross" src="cross.png" />`;
 
-   return `
-      <div class="cell row-${row} col-${column}" id="${id}">
-      </div>
-   `;
+      if (symbol == 0) imgString = `<img class="circle" src="circle.png" />`;
+
+      const winnerCell = winnerCells.some((x) => x.id == cell.id);
+
+      return `
+         <div class="cell ${winnerCell ? 'winner-cell' : ''} row-${row} col-${column}" id="${id}">
+            ${imgString}
+         </div>
+      `;
+   }
 }
 
 function drawTurns() {
@@ -75,10 +104,6 @@ function drawTurns() {
    `;
 }
 
-//Turno. true = X, false = O
-var currentTurn;
-var hasGameStarted = false;
-
 export function threeInRowListeners() {
    //Elegir turno
    const circleStart = document.querySelector('#circle');
@@ -86,19 +111,25 @@ export function threeInRowListeners() {
    const title = document.querySelector('.threeinrow-turns h3');
 
    circleStart.addEventListener('click', (event) => {
-      currentTurn = false;
+      currentTurn = 0;
       hasGameStarted = true;
 
       crossStart.classList.add('d-none');
       title.textContent = 'Turno de';
+
+      localStorage.setItem('TIR-gameStarted', hasGameStarted);
+      localStorage.setItem('TIR-currentTurn', currentTurn);
    });
 
    crossStart.addEventListener('click', (event) => {
-      currentTurn = true;
+      currentTurn = 1;
       hasGameStarted = true;
 
       circleStart.classList.add('d-none');
       title.textContent = 'Turno de';
+
+      localStorage.setItem('TIR-gameStarted', hasGameStarted);
+      localStorage.setItem('TIR-currentTurn', currentTurn);
    });
 
    //Poner una cruz o un círculo.
@@ -108,40 +139,168 @@ export function threeInRowListeners() {
          const id = event.target.id;
          const currentCell = currentGame.find((c) => c.id == id);
 
+         console.log(typeof hasGameStarted);
+
          if (!currentCell) {
             return;
          }
 
          if (hasGameStarted) {
-            if (currentTurn == true) {
+            if (currentTurn == 1) {
                cell.innerHTML = `
                   <img class="cross" src="cross.png" />
                 `;
-            } else {
+            } else if (currentTurn == 0) {
                cell.innerHTML = `
                 <img class="circle" src="circle.png" />
                 `;
             }
 
-            const classes = event.target.classList.value.split(' ');
-            const row = classes[1].split('-')[1];
-            const col = classes[2].split('-')[1];
-
-            checkWin(currentTurn, row, col);
+            const cellId = event.target.id.split('-')[1];
 
             currentCell.symbol = currentTurn;
-            currentTurn = !currentTurn;
 
+            checkWin(currentTurn, cellId);
+
+            currentTurn = currentTurn == 0 ? 1 : 0;
             crossStart.classList.toggle('d-none');
             circleStart.classList.toggle('d-none');
+
+            localStorage.setItem('TIR-currentTurn', currentTurn);
+            localStorage.setItem('TIR-currentGame', JSON.stringify(currentGame));
          }
       });
    });
 }
 
-const winningMap = ['123', '456', '789', '147', '258', '369', '159', '357'];
-
 //player = true - X, player = false - O
-function checkWin(player, row, column) {
+function checkWin(player, cellId) {
    //Chequear ganador.
+   const possibleWins = winningMap.filter((x) => x.includes(cellId));
+
+   for (const possibleWin of possibleWins) {
+      const matchingCells = possibleWin.split('');
+
+      //No resto 1 al index porque luego lo uso para hacer una query con los IDs
+      //de las celdas. El index 0 corresponde a "cell-1", y así sucesivamente.
+      const firstIndex = matchingCells[0];
+      const secondIndex = matchingCells[1];
+      const thirdIndex = matchingCells[2];
+
+      const firstWinningCell = currentGame[firstIndex - 1];
+      const secondWinningCell = currentGame[secondIndex - 1];
+      const thirdWinningCell = currentGame[thirdIndex - 1];
+
+      //El 1 lo resto manualmente aquí, para luego tener el index limpio en la query.
+      const firstCellSymbol = firstWinningCell.symbol;
+      const secondCellSymbol = secondWinningCell.symbol;
+      const thirdCellSymbol = thirdWinningCell.symbol;
+
+      if (firstCellSymbol == player && firstCellSymbol != -1 && secondCellSymbol == player && thirdCellSymbol == player) {
+         const firstCellDiv = document.querySelector(`#cell-${firstIndex}`);
+         const secondCellDiv = document.querySelector(`#cell-${secondIndex}`);
+         const thirdCellDiv = document.querySelector(`#cell-${thirdIndex}`);
+
+         firstCellDiv.classList.add('winner-cell');
+         secondCellDiv.classList.add('winner-cell');
+         thirdCellDiv.classList.add('winner-cell');
+
+         winnerCells.push(firstWinningCell);
+         winnerCells.push(secondWinningCell);
+         winnerCells.push(thirdWinningCell);
+
+         localStorage.setItem('TIR-winnerCells', JSON.stringify(winnerCells));
+
+         finishGame(player);
+         return;
+      }
+   }
+}
+
+function finishGame(winner) {
+   //Reiniciamos las variables del juego y añadimos el punto
+   currentTurn = -1;
+   hasGameStarted = false;
+
+   localStorage.setItem('TIR-currentTurn', currentTurn);
+   localStorage.setItem('TIR-gameStarted', hasGameStarted);
+
+   //Cambiar aspectos visuales
+   if (winner == 1) {
+      var score = document.querySelector('.player-2 p');
+      crossScore++;
+      score.textContent = crossScore;
+      localStorage.setItem('TIR-crossScore', crossScore);
+   } else {
+      var score = document.querySelector('.player-1 p');
+      circleScore++;
+      score.textContent = circleScore;
+      localStorage.setItem('TIR-circleScore', circleScore);
+   }
+
+   const turnsDiv = document.querySelector('.threeinrow-turns');
+
+   //Lo hago de esta manera para poder meter el listener en "restart".
+   const winnerDiv = document.createElement('div');
+   const winnerTitle = document.createElement('h3');
+   const winnerImg = document.createElement('img');
+   const restartButton = document.createElement('a');
+
+   restartButton.addEventListener('click', (event) => {
+      currentGame.splice(0, currentGame.length);
+
+      resetGame();
+      const game = document.querySelector('.current-game');
+      game.innerHTML = `${threeInRowGame()}`;
+      threeInRowListeners();
+
+      localStorage.setItem('TIR-currentGame', JSON.stringify(currentGame));
+   });
+
+   winnerTitle.textContent = 'Ha ganado el jugador';
+   winnerImg.src = winner == 1 ? 'cross.png' : 'circle.png';
+   restartButton.textContent = 'Jugar otra vez';
+
+   winnerImg.classList.add(winner == 1 ? 'cross' : 'circle');
+   winnerDiv.classList.add('threeinrow-winner');
+   restartButton.classList.add('dir-restart');
+
+   winnerDiv.appendChild(winnerTitle);
+   winnerDiv.appendChild(winnerImg);
+   winnerDiv.appendChild(restartButton);
+
+   turnsDiv.replaceChildren(winnerDiv);
+}
+
+function loadGame() {
+   circleScore = localStorage.getItem('TIR-circleScore') || 0;
+   crossScore = localStorage.getItem('TIR-crossScore') || 0;
+
+   currentTurn = localStorage.getItem('TIR-currentTurn') || -1;
+   hasGameStarted = JSON.parse(localStorage.getItem('TIR-gameStarted')) || false;
+
+   const currentGameData = localStorage.getItem('TIR-currentGame');
+   const currentGameParsedData = JSON.parse(currentGameData);
+
+   currentGame = currentGameParsedData ? currentGameParsedData : [];
+
+   const winnerCellsData = localStorage.getItem('TIR-winnerCells');
+   const winnerCellParsedData = JSON.parse(winnerCellsData);
+
+   winnerCells = winnerCellParsedData ? winnerCellParsedData : [];
+
+   //cargar aspecto del último panel
+   //cargar los listeners
+}
+
+function resetGame() {
+   currentTurn = -1;
+   hasGameStarted = false;
+   currentGame = [];
+   winnerCells = [];
+
+   localStorage.setItem('TIR-currentTurn', currentTurn);
+   localStorage.setItem('TIR-gameStarted', hasGameStarted);
+   localStorage.setItem('TIR-currentGame', JSON.stringify(currentGame));
+   localStorage.setItem('TIR-winnerCells', JSON.stringify(winnerCells));
 }
